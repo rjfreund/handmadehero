84cc213e3ce022b4ab5c287d159b8d015d7c68d5
R.J. Freund <robert.joseph.freund@gmail.com>
2019-12-04 15:45:13 -0600

    updates

--- handmadehero/handemade.cpp
+++ handmadehero/handemade.cpp
@@ -2,7 +2,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <Xinput.h>
+#include <dsound.h>
 #pragma comment(lib, "XInput.lib")
+#pragma comment(lib, "dsound.lib")
 //win32
 
 BITMAPINFO bitmapInfo;
@@ -10,6 +12,44 @@ void *bitmapMemory;
 int bitmapWidth;
 int bitmapHeight;
 int bytesPerPixel = 4;
+
+LPDIRECTSOUNDBUFFER secondaryAudioBuffer;
+
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
+
+void initializeDirectSound(HWND window, int32_t samplesPerSecond, int32_t bufferSize) {
+	HMODULE directSoundLibrary = LoadLibrary("dsound.dll");
+	if (!directSoundLibrary) { return; /*error*/ }
+	direct_sound_create *directSoundCreate = (direct_sound_create *)GetProcAddress(directSoundLibrary, "DirectSoundCreate");
+	LPDIRECTSOUND directSound;
+	if (!SUCCEEDED(DirectSoundCreate(0, &directSound, 0))) { return; /* log error maybe */ }
+	if (!SUCCEEDED(directSound->SetCooperativeLevel(window, DSSCL_PRIORITY))) { return; /*log error*/ }
+	DSBUFFERDESC bufferDescription = {};
+	bufferDescription.dwSize = sizeof(bufferDescription);
+	bufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+	LPDIRECTSOUNDBUFFER primaryBuffer;
+	if (!SUCCEEDED(directSound->CreateSoundBuffer(&bufferDescription, &primaryBuffer, 0))) { return; }
+
+	WAVEFORMATEX waveFormat = {};
+	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+	waveFormat.nChannels = 2;
+	waveFormat.nSamplesPerSec = samplesPerSecond;
+	waveFormat.wBitsPerSample = 16;
+	waveFormat.nBlockAlign = (waveFormat.nChannels * waveFormat.wBitsPerSample) / 8;
+	waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+	waveFormat.cbSize = 0;
+	if (!SUCCEEDED(primaryBuffer->SetFormat(&waveFormat))) { return; };
+
+	DSBUFFERDESC secondaryBufferDescription = {};
+	secondaryBufferDescription.dwSize = sizeof(secondaryBufferDescription);
+	secondaryBufferDescription.dwFlags = 0;
+	secondaryBufferDescription.dwBufferBytes = bufferSize;
+	secondaryBufferDescription.lpwfxFormat = &waveFormat;
+
+	if (!SUCCEEDED(directSound->CreateSoundBuffer(&secondaryBufferDescription, &secondaryAudioBuffer, 0))) { return; }
+}
 
 void render(int xOffset, int yOffset)
 {
@@ -49,8 +89,8 @@ void render(int xOffset, int yOffset)
 
 void resizeDeviceIndependantBitmapSection(int width, int height) {
 
-	if (bitmapMemory) 
-	{ 
+	if (bitmapMemory)
+	{
 		VirtualFree(
 			/*LPVOID lpAddress*/	bitmapMemory,
 			/*SIZE_T dwSize*/		0,
@@ -75,105 +115,105 @@ void resizeDeviceIndependantBitmapSection(int width, int height) {
 		/*SIZE_T dwSize*/			bitmapMemorySize,
 		/*DWORD  flAllocationType*/	MEM_COMMIT,
 		/*DWORD  flProtect*/		PAGE_READWRITE
-	);		
+	);
 }
 
-void updateWindow(HDC deviceContext, RECT clientRectangle, int x, int y, int width, int height) 
+void updateWindow(HDC deviceContext, RECT clientRectangle, int x, int y, int width, int height)
 {
 	int windowWidth = clientRectangle.right - clientRectangle.left;
 	int windowHeight = clientRectangle.bottom - clientRectangle.top;
 
 	StretchDIBits(
-	  /*HDC              hdc*/			deviceContext,
-	  /*int              xDest*/		0,
-	  /*int              yDest*/		0,
-	  /*int              DestWidth*/	windowWidth,
-	  /*int              DestHeight*/	windowHeight,
-	  /*int              xSrc*/			0,
-	  /*int              ySrc*/			0,
-	  /*int              SrcWidth*/		bitmapWidth,
-	  /*int              SrcHeight*/	bitmapHeight,
-	  /*const VOID       *lpBits*/		bitmapMemory,
-	  /*const BITMAPINFO *lpbmi*/		&bitmapInfo,
-	  /*UINT             iUsage*/		DIB_RGB_COLORS,
-	  /*DWORD            rop*/			SRCCOPY
+		/*HDC              hdc*/			deviceContext,
+		/*int              xDest*/		0,
+		/*int              yDest*/		0,
+		/*int              DestWidth*/	windowWidth,
+		/*int              DestHeight*/	windowHeight,
+		/*int              xSrc*/			0,
+		/*int              ySrc*/			0,
+		/*int              SrcWidth*/		bitmapWidth,
+		/*int              SrcHeight*/	bitmapHeight,
+		/*const VOID       *lpBits*/		bitmapMemory,
+		/*const BITMAPINFO *lpbmi*/		&bitmapInfo,
+		/*UINT             iUsage*/		DIB_RGB_COLORS,
+		/*DWORD            rop*/			SRCCOPY
 	);
 }
 
-LRESULT CALLBACK HandleWindowMessage(_In_ HWND window,_In_ UINT message, _In_ WPARAM wParam,_In_ LPARAM lParam) 
+LRESULT CALLBACK HandleWindowMessage(_In_ HWND window, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam)
 {
 	LRESULT Result = 0;
 	switch (message)
 	{
-		case WM_SIZE:
-		{			
-		} break;
-		case WM_PAINT:
-		{
-			PAINTSTRUCT paint;
-			HDC deviceContext = BeginPaint(window, &paint);
-			int x = paint.rcPaint.left;
-			int y = paint.rcPaint.top;
-			int width = paint.rcPaint.right - paint.rcPaint.left;
-			int height = paint.rcPaint.bottom - paint.rcPaint.top;
+	case WM_SIZE:
+	{
+	} break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT paint;
+		HDC deviceContext = BeginPaint(window, &paint);
+		int x = paint.rcPaint.left;
+		int y = paint.rcPaint.top;
+		int width = paint.rcPaint.right - paint.rcPaint.left;
+		int height = paint.rcPaint.bottom - paint.rcPaint.top;
 
-			RECT clientRect;
-			GetClientRect(window, &clientRect);
-			updateWindow(deviceContext, clientRect, x, y, width, height);
-			EndPaint(window, &paint);
-		} break;
-		case WM_DESTROY: 
-		{
-			OutputDebugStringA("WM_DESTROY");
-		} break;
-		case WM_CLOSE:
-		{
-			//DestroyWindow(window);
-			Result = DefWindowProc(window, message, wParam, lParam);
-		} break;
-		case WM_ACTIVATEAPP: 
-		{
-			OutputDebugStringA("WM_ACTIVATEAPP");
-		} break;
-		case WM_SYSKEYDOWN:
-		case WM_SYSKEYUP:
-		case WM_KEYDOWN:
-		case WM_KEYUP:
-		{
-			uint32_t virtualKeyCode = wParam;
-			bool wasDownPrev = (lParam & (1 << 30)) != 0;
-			bool isDown = (lParam & (1 << 31)) == 0;
-			if (isDown == wasDownPrev) { break; }
-			switch (virtualKeyCode) {
-				case 'W':
-				case 'A':
-				case 'S':
-				case 'D':
-				case 'Q':
-				case 'E':
-				case VK_UP:
-				case VK_DOWN:
-				case VK_LEFT:
-				case VK_RIGHT:				
-				case VK_ESCAPE:
-				case VK_SPACE:
-					TCHAR virtualKeyCodeString[256];
-					sprintf_s(virtualKeyCodeString, "%c", virtualKeyCode);
-					OutputDebugStringA(virtualKeyCodeString);
-					OutputDebugStringA("\n");
-					break;
-			}			
-		} break;
-		default: 
-		{
-			Result = DefWindowProc(window, message, wParam, lParam);
-		} break;
+		RECT clientRect;
+		GetClientRect(window, &clientRect);
+		updateWindow(deviceContext, clientRect, x, y, width, height);
+		EndPaint(window, &paint);
+	} break;
+	case WM_DESTROY:
+	{
+		OutputDebugStringA("WM_DESTROY");
+	} break;
+	case WM_CLOSE:
+	{
+		//DestroyWindow(window);
+		Result = DefWindowProc(window, message, wParam, lParam);
+	} break;
+	case WM_ACTIVATEAPP:
+	{
+		OutputDebugStringA("WM_ACTIVATEAPP");
+	} break;
+	case WM_SYSKEYDOWN:
+	case WM_SYSKEYUP:
+	case WM_KEYDOWN:
+	case WM_KEYUP:
+	{
+		uint32_t virtualKeyCode = wParam;
+		bool wasDownPrev = (lParam & (1 << 30)) != 0;
+		bool isDown = (lParam & (1 << 31)) == 0;
+		if (isDown == wasDownPrev) { break; }
+		switch (virtualKeyCode) {
+		case 'W':
+		case 'A':
+		case 'S':
+		case 'D':
+		case 'Q':
+		case 'E':
+		case VK_UP:
+		case VK_DOWN:
+		case VK_LEFT:
+		case VK_RIGHT:
+		case VK_ESCAPE:
+		case VK_SPACE:
+			TCHAR virtualKeyCodeString[256];
+			sprintf_s(virtualKeyCodeString, "%c", virtualKeyCode);
+			OutputDebugStringA(virtualKeyCodeString);
+			OutputDebugStringA("\n");
+			break;
+		}
+	} break;
+	default:
+	{
+		Result = DefWindowProc(window, message, wParam, lParam);
+	} break;
 	}
 
 	return Result;
 }
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nShowCmd) {
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 	WNDCLASS wc = { };
 	wc.lpfnWndProc = HandleWindowMessage;
 	wc.hInstance = hInstance;
@@ -197,7 +237,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLin
 		/*LPVOID    lpParam*/ 		NULL
 	);
 
-	if (window == NULL){ return 0; }
+	if (window == NULL) { return 0; }
 
 	RECT clientRect;
 	GetClientRect(window, &clientRect);
@@ -212,16 +252,34 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLin
 	MSG message = { };
 	int xOffset = 0;
 	int yOffset = 0;
+
+	int samplesPerSecond = 48000;
+	int toneHz = 256;
+	int toneVolume = 500;
+	uint32_t runningSampleIndex = 0;
+	int squareWavePeriod = samplesPerSecond / toneHz;
+	int halfSquareWavePeriod = squareWavePeriod / 2;
+	int bytesPerSample = sizeof(int16_t) * 2;
+	int secondaryBufferSize = samplesPerSecond * bytesPerSample;
+
+
+	initializeDirectSound(window, samplesPerSecond, samplesPerSecond * bytesPerSample);
+	secondaryAudioBuffer->Play(
+		/*DWORD dwReserved1*/	0,
+		/*DWORD dwPriority*/	0,
+		/*DWORD dwFlags*/		DSBPLAY_LOOPING
+	);
+
 	while (1)
 	{
 		if (message.message == WM_QUIT) { break; }
-		if (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) { 
+		if (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
 
 		for (int i = 0; i < XUSER_MAX_COUNT; i++) {
-			XINPUT_STATE controllerState;			
+			XINPUT_STATE controllerState;
 			if (XInputGetState(i, &controllerState) != ERROR_SUCCESS) {
 				//controller not plugged in, maybe log error
 				continue;
@@ -244,10 +302,71 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLin
 			int16_t stickY = gamepad->sThumbLY;
 
 			if (aButton) {}
-			
+
 		}
 
 		render(xOffset, yOffset);
+
+		DWORD playCursor;
+		DWORD writeCursor;
+		if (!SUCCEEDED(secondaryAudioBuffer->GetCurrentPosition(&playCursor, &writeCursor))) { return 0; }
+
+		DWORD byteToLock = (runningSampleIndex * bytesPerSample) % secondaryBufferSize;
+		DWORD bytesToWrite;
+		if (byteToLock > playCursor) {
+			bytesToWrite = secondaryBufferSize - byteToLock;
+			bytesToWrite += playCursor;
+		}
+		else {
+			bytesToWrite = playCursor - byteToLock;
+		}
+
+		void * region1;
+		DWORD region1Size;
+		void * region2;
+		DWORD region2Size;
+
+		secondaryAudioBuffer->Lock(
+			/*DWORD dwOffset*/			byteToLock,
+			/*DWORD dwBytes*/			bytesToWrite,
+			/*LPVOID * ppvAudioPtr1*/	&region1,
+			/*LPDWORD pdwAudioBytes1*/	&region1Size,
+			/*LPVOID * ppvAudioPtr2*/	&region2,
+			/*LPDWORD pdwAudioBytes2*/	&region2Size,
+			/*DWORD dwFlags*/			0
+		);
+
+		int16_t *sampleOut = (int16_t *)region1;
+		DWORD region1SampleCount = region1Size / bytesPerSample;
+
+		for (DWORD sampleIndex = 0; sampleIndex < region1SampleCount; sampleIndex++) {
+			int16_t sampleValue = ((runningSampleIndex / halfSquareWavePeriod) % 2) ? toneVolume : -toneVolume;
+			*sampleOut = sampleValue;
+			*sampleOut++;
+			*sampleOut = sampleValue;
+			*sampleOut++;
+			runningSampleIndex++;
+		}
+
+		sampleOut = (int16_t *)region2;
+		DWORD region2SampleCount = region2Size / bytesPerSample;
+		for (DWORD sampleIndex = 0; sampleIndex < region2SampleCount; sampleIndex++) {
+			int16_t sampleValue = ((runningSampleIndex / halfSquareWavePeriod) % 2) ? toneVolume : -toneVolume;
+			*sampleOut = sampleValue;
+			*sampleOut++;
+			*sampleOut = sampleValue;
+			*sampleOut++;
+			runningSampleIndex++;
+		}
+
+		secondaryAudioBuffer->Unlock(
+			/*LPVOID pvAudioPtr1*/	region1,
+			/*DWORD dwAudioBytes1*/	region1Size,
+			/*LPVOID pvAudioPtr2*/	region2,
+			/*DWORD dwAudioBytes2*/	region2Size
+		);
+
+
 		HDC deviceContext = GetDC(window);
 
 		RECT clientRectangle;
@@ -256,7 +375,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLin
 		int windowHeight = clientRectangle.bottom - clientRectangle.top;
 		updateWindow(deviceContext, clientRectangle, 0, 0, windowWidth, windowHeight);
 		ReleaseDC(window, deviceContext);
-		
+
 		xOffset++;
 	}
 	return 0;
